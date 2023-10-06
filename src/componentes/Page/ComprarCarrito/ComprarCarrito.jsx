3ef51e582212cff8743543f2cb1e546c326300c5
Sebastian Lanzetti <slanzetti@lanus.gob.ar>
2023-10-06 10:22:23 -0300

    intento de local + firebase 4

--- src/componentes/Page/ComprarCarrito/ComprarCarrito.jsx
+++ src/componentes/Page/ComprarCarrito/ComprarCarrito.jsx
@@ -1,37 +1,44 @@
-
-import React from 'react';
-import SweetCompra from '../../sweetAlert/SweetCompra';
-import {useState} from 'react';
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 
 const ComprarCarrito = ({ cartItems }) => {
-    const [showSweetCompra, setShowSweetCompra] = useState(false);
-    const [compraId, setCompraId] = useState(null);
-  
-    const handleComprar = () => {
-        // Generar un ID único para la compra
-        const newCompraId = `compra_${Math.random().toString(36).substr(2, 9)}`;
-        setCompraId(newCompraId);
-        const compraItems = [...cartItems];
-        localStorage.setItem(newCompraId, JSON.stringify(compraItems));
+  const [compraRealizada, setCompraRealizada] = useState(false);
 
-        localStorage.removeItem('carrito');
-        setShowSweetCompra(true);
-    };
-  
-    const handleCloseSweetCompra = () => {
-      // Ocultar el modal SweetCompra y limpiar el ID
-      setShowSweetCompra(false);
-      setCompraId(null);
-    };
-  
-    return (
-      <div>
-        <button onClick={handleComprar}>Comprar</button>
-        {showSweetCompra && (
-          <SweetCompra compraId={compraId} onClose={handleCloseSweetCompra} />
-        )}
-      </div>
-    );
+  const handleComprar = () => {
+    // Comprobar si la compra ya se realizó
+    if (compraRealizada) {
+      return;
+    }
+
+    // Generar un ID único para la compra
+    const newCompraId = `compra_${Math.random().toString(36).substr(2, 9)}`;
+
+    // Realizar otras acciones relacionadas con la compra aquí si es necesario
+
+    // Limpiar el carrito actual (opcional)
+    localStorage.removeItem('carrito');
+
+    // Actualizar el estado para deshabilitar el botón
+    setCompraRealizada(true);
+
+    // Mostrar SweetAlert
+    Swal.fire({
+      title: 'Compra generada',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+    }).then(() => {
+      // Recargar la página después de cerrar SweetAlert
+      window.location.reload();
+    });
   };
-  
-  export default ComprarCarrito;
+
+  return (
+    <div>
+      <button onClick={handleComprar} disabled={compraRealizada}>
+        {compraRealizada ? 'Compra Realizada' : 'Comprar'}
+      </button>
+    </div>
+  );
+};
+
+export default ComprarCarrito;
