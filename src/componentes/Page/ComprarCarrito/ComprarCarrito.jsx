f4189d024f11cb0c3fd1afcd98f7ea48703b93e4
Sebastian Lanzetti <slanzetti@lanus.gob.ar>
2023-10-06 10:38:03 -0300

    intento de local + firebase 5

--- src/componentes/Page/ComprarCarrito/ComprarCarrito.jsx
+++ src/componentes/Page/ComprarCarrito/ComprarCarrito.jsx
@@ -2,29 +2,40 @@ import React, { useState } from 'react';
 import SweetCompra from '../../sweetAlert/SweetCompra';
 
 const ComprarCarrito = ({ cartItems }) => {
-    const [showSweetCompra, setShowSweetCompra] = useState(false);
-    const handleComprar = () => {
-        if (showSweetCompra) {
-        return;
-        }
-        setShowSweetCompra(true);
-    };
-    const handleCloseSweetCompra = () => {
-        setShowSweetCompra(false);
-        localStorage.removeItem('carrito');
-        window.location.reload();
-        };
+  const [showSweetCompra, setShowSweetCompra] = useState(false);
 
-    return (
-        <div>
-        <button onClick={handleComprar} disabled={showSweetCompra}>
-            {showSweetCompra ? 'Compra Realizada' : 'Comprar'}
-        </button>
-        {showSweetCompra && (
-            <SweetCompra onClose={handleCloseSweetCompra} />
-        )}
-        </div>
-    );
+  const handleComprar = () => {
+    // Comprobar si la compra ya se realizó
+    if (showSweetCompra) {
+      return;
+    }
+
+    // Generar un identificador único para la compra (por ejemplo, un timestamp)
+    const compraId = `compra_${Date.now()}`;
+
+    // Guardar la compra en el localStorage con el identificador único
+    localStorage.setItem(compraId, JSON.stringify(cartItems));
+
+    // Mostrar SweetCompra al hacer clic en el botón de "Comprar"
+    setShowSweetCompra(true);
+  };
+
+  const handleCloseSweetCompra = () => {
+    setShowSweetCompra(false);
+    localStorage.removeItem('carrito');
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      <button onClick={handleComprar} disabled={showSweetCompra}>
+        {showSweetCompra ? 'Compra Realizada' : 'Comprar'}
+      </button>
+      {showSweetCompra && (
+        <SweetCompra onClose={handleCloseSweetCompra} />
+      )}
+    </div>
+  );
 };
 
 export default ComprarCarrito;
