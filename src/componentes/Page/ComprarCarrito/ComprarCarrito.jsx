efacf60e05a8c650b4372c5874eac3ab43b9d7c9
Sebastian Lanzetti <slanzetti@lanus.gob.ar>
2023-10-06 10:26:07 -0300

    intento de local + firebase 4

--- src/componentes/Page/ComprarCarrito/ComprarCarrito.jsx
+++ src/componentes/Page/ComprarCarrito/ComprarCarrito.jsx
@@ -1,44 +1,30 @@
 import React, { useState } from 'react';
-import Swal from 'sweetalert2';
+import SweetCompra from '../../sweetAlert/SweetCompra';
 
 const ComprarCarrito = ({ cartItems }) => {
-  const [compraRealizada, setCompraRealizada] = useState(false);
+    const [showSweetCompra, setShowSweetCompra] = useState(false);
+    const handleComprar = () => {
+        if (showSweetCompra) {
+        return;
+        }
+        setShowSweetCompra(true);
+    };
+    const handleCloseSweetCompra = () => {
+        setShowSweetCompra(false);
+        localStorage.removeItem('carrito');
+        window.location.reload();
+        };
 
-  const handleComprar = () => {
-    // Comprobar si la compra ya se realizó
-    if (compraRealizada) {
-      return;
-    }
-
-    // Generar un ID único para la compra
-    const newCompraId = `compra_${Math.random().toString(36).substr(2, 9)}`;
-
-    // Realizar otras acciones relacionadas con la compra aquí si es necesario
-
-    // Limpiar el carrito actual (opcional)
-    localStorage.removeItem('carrito');
-
-    // Actualizar el estado para deshabilitar el botón
-    setCompraRealizada(true);
-
-    // Mostrar SweetAlert
-    Swal.fire({
-      title: 'Compra generada',
-      icon: 'success',
-      confirmButtonText: 'Aceptar',
-    }).then(() => {
-      // Recargar la página después de cerrar SweetAlert
-      window.location.reload();
-    });
-  };
-
-  return (
-    <div>
-      <button onClick={handleComprar} disabled={compraRealizada}>
-        {compraRealizada ? 'Compra Realizada' : 'Comprar'}
-      </button>
-    </div>
-  );
+    return (
+        <div>
+        <button onClick={handleComprar} disabled={showSweetCompra}>
+            {showSweetCompra ? 'Compra Realizada' : 'Comprar'}
+        </button>
+        {showSweetCompra && (
+            <SweetCompra onClose={handleCloseSweetCompra} />
+        )}
+        </div>
+    );
 };
 
 export default ComprarCarrito;
